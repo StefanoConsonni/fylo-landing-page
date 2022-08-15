d659d41e2997551aacd0b2d4fd10b2fadb7afae2
Stefano Consonni <stefanoconsonni@ymail.com>
2022-08-15 16:46:03 +0200

    add sticky bar navigation

--- js/script.js
+++ js/script.js
@@ -24,3 +24,29 @@ allLinks.forEach(function (link) {
 		// if (link.classList.contains("main-nav-link")) headerEl.classList.toggle("nav-open");
 	});
 });
+
+///////////////////////////////////////////////////////////
+// Sticky Navbar
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(
+	function (entries) {
+		const entry = entries[0];
+
+		if (entry.isIntersecting === false) {
+			document.body.classList.add("sticky");
+		}
+
+		if (entry.isIntersecting === true) {
+			document.body.classList.remove("sticky");
+		}
+	},
+	{
+		// In the viewport
+		root: null,
+		threshold: 0,
+		rootMargin: "-80px",
+	}
+);
+observer.observe(sectionHeroEl);
